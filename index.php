4c26faabfddf7006dc96510f0c1686b701abebcb
pacitwizere <pacitwizere@hotmail.com>
2020-03-28 18:24:22 +0800

    Mu gihugu hose

--- index.php
+++ index.php
@@ -7,12 +7,20 @@ $text = $_POST["text"];
 //This is the first menu screen
 
 if ( $text == "" ) {
-  $response  = "Murakaza neza, Murabasha kubona amakuru ajyanye n'icyorezo cya korona virus mu Rwanda \n Reba umubare w'abanduye:";
-  $response .= "1. Mu gihugu Hose \n";
+  $response  = "Murakaza neza, Murabasha kubona amakuru ajyanye n'icyorezo cya korona virus mu Rwanda \n Reba umubare w'abanduye: \n";
+  $response .= "1. Mu gihugu hose \n";
   $response .= "2. Mu ntara urimo \n";
   $response .= "3. Mu Karere urimo \n";
   $response .= "4. Mu murenge urimo \n";
 }
+
+else if ($text == "1") {
+  $response  = "Kuwa 28 Werurwe 2020 saa 18:21\n Mugihugu hose hamaze kwandura abantu 50\n\n";
+  $response .= "1.Mu ntara urimo\n";
+  $response .= "2.Mu zindi ntara\n";
+  $response .= "\n0.Ahabanza\n";
+}
+
 
 
 //echo response
